172fe9fadcce9319291f2cc59a4460ab766d6747
apan1121 <apan1121@gmail.com>
2019-01-05 17:48:39 +0800

    [fix] 修正 mixpanel

--- src/js/lib/common/util/mixpanel.js
+++ src/js/lib/common/util/mixpanel.js
@@ -5,6 +5,7 @@ new Fingerprint2().get(function(result, components) {
     let identify = new Fingerprint2().x64hash128(components.map(function(pair) { return pair.value }).join(), 31);
     mixpanel_modul.data.identify = identify;
     mixpanel.identify(identify);
+    mixpanel_modul.actWaitFunc();
 });
 
 const mixpanel_modul = {
@@ -13,9 +14,27 @@ const mixpanel_modul = {
         tabId: string.getRandomString(10),
         data: null,
     },
+    waitFunc: [],
+    actWaitFunc: function(){
+        if (mixpanel_modul.waitFunc.length >= 0) {
+            console.log("actWaitFunc", mixpanel_modul.waitFunc);
+            mixpanel_modul.waitFunc.forEach(function(actFunc){
+                actFunc();
+            });
+        }
+    },
     track: function(action, inputData) {
-        let data = { ...mixpanel_modul.data, data: inputData };
-        mixpanel.track(action, data);
+
+        let actionFunc = function(){
+            let data = { ...mixpanel_modul.data, data: inputData };
+            mixpanel.track(action, data);
+        };
+
+        if (!!mixpanel_modul.data.identify) {
+            actionFunc();
+        } else {
+            mixpanel_modul.waitFunc.push(actionFunc);
+        }
     }
 };
 export default mixpanel_modul;
